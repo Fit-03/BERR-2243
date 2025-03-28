0297b19e6fe097de04c1e94c660097ddd773a823
Fit-03 <b122410708@student.utem.edu.my>
2025-03-29 03:36:43 +0800

    updated week-2 index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// This is week 2 exercise
 const {MongoClient} = require('mongodb');
 
 const drivers = [
@@ -30,6 +31,9 @@ const drivers = [
     });
 
     async function main() {
+        const uri = "mongodb://localhost:27017/"
+        const client = new MongoClient(uri);
+        
         try {
             await client.connect();
             const db = client.db('testDB');
