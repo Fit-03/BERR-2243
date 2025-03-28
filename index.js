440b4ebd7e32aabaa46c156368f0f3d252724c73
Fit-03 <b122410708@student.utem.edu.my>
2025-03-29 03:07:01 +0800

    updated week-2 index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const {MongoClient} = require('mongodb');
+
+const drivers = [
+    {
+        name: "John Doe",
+        vehicleType: "Sedan",
+        isAvailable: true,
+        rating: 4.8,
+    },
+    {
+        name: "Alice Smith",
+        vehicleType: "SUV",
+        isAvailable: false,
+        rating: 4.5,
+    }
+];
+
+    // Show the data in the console
+    console.log(drivers);
+
+    // Show the all the drivers name in the console
+    drivers.forEach(driver => {console.log(driver.name);});
+
+    // Add additional driver to the drivers array
+    drivers.push({
+        name: "Bob Johnson",
+        vehicleType: "Truck",
+        isAvailable: true,
+        rating: 4.7,
+    });
+
+    async function main() {
+        try {
+            await client.connect();
+            const db = client.db('testDB');
+
+            const driversCollection = db.collection('drivers');
+
+            drivers.forEach(async (driver) => {
+                const result = await driversCollection.insertOne(driver);
+                console.log(`Inserted driver with id: ${result.insertedId}`);
+            });
+        }
+
+        finally {
+            await client.close();
+        }
+    }
