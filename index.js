07c19845b0c8f553431760189fc42bc18c77689f
Fit-03 <b122410708@student.utem.edu.my>
2025-04-09 22:25:38 +0800

    Updated week-3 index.js step 1

--- index.js
+++ index.js
@@ -1,52 +1,27 @@
-// This is week 2 exercise
-const {MongoClient} = require('mongodb');
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const port = 3000;
 
-const drivers = [
-    {
-        name: "John Doe",
-        vehicleType: "Sedan",
-        isAvailable: true,
-        rating: 4.8,
-    },
-    {
-        name: "Alice Smith",
-        vehicleType: "SUV",
-        isAvailable: false,
-        rating: 4.5,
-    }
-];
+const app = express();
+app.use(express.json());
 
-    // Show the data in the console
-    console.log(drivers);
+let db;
 
-    // Show the all the drivers name in the console
-    drivers.forEach(driver => {console.log(driver.name);});
+async function connecToMongoDB() {
+    const uri = 'mongodb://localhost:27017/';
+    const client = new MongoClient(uri);
 
-    // Add additional driver to the drivers array
-    drivers.push({
-        name: "Bob Johnson",
-        vehicleType: "Truck",
-        isAvailable: true,
-        rating: 4.7,
-    });
-
-    async function main() {
-        const uri = "mongodb://localhost:27017/"
-        const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
         
-        try {
-            await client.connect();
-            const db = client.db('testDB');
-
-            const driversCollection = db.collection('drivers');
-
-            drivers.forEach(async (driver) => {
-                const result = await driversCollection.insertOne(driver);
-                console.log(`New driver created with result: ${result}`);
-            });
-        }
-
-        finally {
-            await client.close();
-        }
+        db = client.db('testDB');
+    } catch (error) {
+        console.error('Error:', error);
     }
+}
+connecToMongoDB();
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
