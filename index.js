fc237d3c84866690810e441d602590ae4208e6b4
Fit-03 <b122410708@student.utem.edu.my>
2025-04-09 22:41:45 +0800

    Updated week-3 index.js PATCH

--- index.js
+++ index.js
@@ -45,3 +45,21 @@ app.post('/rides', async (req, res) => {
         res.status(500).json({error: "Invalid ride data"});
     }
 });
+
+// PATCH /rides/:id - Update a ride
+app.patch('/rides/:id', async (req, res) => {
+    try {
+        const result = await db.collection('rides').updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: {status}}
+        );
+
+        if (result.modifiedCount === 0) {
+            return res.status(404).json({error: "Ride not found"});
+        }
+        res.status(200).json({ updated: result.modifiedCount });
+
+    } catch (error) {
+        res.status(400).json({error: "Invalid ride ID or data"});
+    }
+});
